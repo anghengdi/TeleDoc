960d14149079af089e3ac439f414fbc55feabe09
Ang Heng Di <hengdi_11@hotmail.com>
2022-03-16 23:19:08 +0800

    Update with requirement.txt

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, redirect, render_template, jsonify, flash, url_for, abort, \
     send_from_directory 
 # from views import *
+# from flask_mysqldb import MySQL
+# from flask_sqlalchemy import SQLAlchemy
 
 from werkzeug.utils import secure_filename
 import os 
@@ -8,7 +10,7 @@ import imghdr
 
 
 app = Flask(__name__)
-app.config['MAX_CONTENT_LENGTH'] = 1024 * 1024
+app.config['MAX_CONTENT_LENGTH'] = 2 * 1024 * 1024
 app.config['UPLOAD_EXTENSIONS'] = ['.jpg', '.png', '.gif']
 app.config['UPLOAD_PATH'] = 'static/uploads'
 
@@ -39,6 +41,10 @@ def videocall() :
 def scan() :
     return render_template("scan.html")
 
+@app.errorhandler(413)
+def too_large(e):
+    return "File is too large", 413
+
 ####Image upload
 def validate_image(stream):
     header = stream.read(512)  # 512 bytes should be enough for a header check
@@ -49,12 +55,12 @@ def validate_image(stream):
     return '.' + (format if format != 'jpeg' else 'jpg')
 
 @app.route('/uploadimagescan')
-def index():
+def uploadimagescan():
     files = os.listdir(app.config['UPLOAD_PATH'])
     return render_template('uploadimagescan.html', files=files)
 
 @app.route('/uploadimagescan', methods=['POST'])
-def upload_file():
+def upload_files():
     uploaded_file = request.files['file']
     filename = secure_filename(uploaded_file.filename)
     if filename != '':
@@ -66,7 +72,7 @@ def upload_file():
     return redirect(url_for('uploadimagescan'))
 
 @app.route('/uploads/<filename>')
-def uploads(filename):
+def upload(filename):
     return send_from_directory(app.config['UPLOAD_PATH'], filename)
 
 
